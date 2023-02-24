87096713d515557fa22052ad2ff4eee8aa38257d
clever-cr <cleverumuhuza@gmail.com>
2023-02-24 21:39:58 +0200

    fix issues

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,20 +4,23 @@
 
 import json
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    def getTodos(id):
-        link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-        res = requests.get(link)
-        return json.loads(res.text)
-    link = "https://jsonplaceholder.typicode.com/users/"
-    res = requests.get(link)
-    users = json.loads(res.text)
-    data = {}
-    for i in users:
-        todos = getTodos(i["id"])
-        data[i["id"]] = [{"task": j["title"], "completed": j["completed"],
-                          "username": i["username"]} for j in todos]
-    with open("todo_all_employees.json", 'w', encoding='utf-8') as f:
-        f.write(json.dumps(data))
+    employee_id = int(sys.argv[1])
+    employee_url = "https://jsonplaceholder\
+.typicode.com/users/{}".format(employee_id)
+    todo_url = "https://jsonplaceholder\
+.typicode.com/users/{}/todos".format(employee_id)
+
+    employee = requests.get(employee_url).json()
+    employee_name = employee['name']
+
+    todo_data = requests.get(todo_url).json()
+    data = [{"task": i["title"],
+             "completed": i["completed"],
+             "username": employee["username"]} for i in todo_data]
+    json_data = json.dumps({"{}".format(employee["id"]): data})
+    with open("{}.json".format(employee["id"]), 'w', encoding='utf-8') as f:
+        f.write(json_data)
